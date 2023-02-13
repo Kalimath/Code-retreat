27d75eb6dc67fa747edae71feffea7c9501a3c73
MB-Development <mathieu_broe@yahoo.com>
2023-02-13 15:35:36 +0100

    clean 2

--- code-retreat/TicTacToeTests.cs
+++ code-retreat/TicTacToeTests.cs
@@ -4,33 +4,10 @@ namespace code_retreat;
 
 public class TicTacToeTests
 {
-    [Fact]
-    public void WhenWeStartAGame_TheBoardShouldBeEmpty()
-    {
-        var newGame = new TicTacToe();
-
-        newGame.Start();
-
-        Assert.Equal(BoardFactory.Create(),newGame.GetBoard());
-    }
+   
 }
 
 public class TicTacToe
 {
-    private char[][] Board { get; set; }
-
-    public void Start() => Board = BoardFactory.Create();
-
-    public char[][] GetBoard()
-    {
-        return Board;
-    }
-}
-
-public static class BoardFactory
-{
-    public static char[][] Create()
-    {
-        return new[] { new char[3], new char[3], new char[3] };
-    }
+    
 }
