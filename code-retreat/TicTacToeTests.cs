a4c18d1d6fd73bfd3f5d78156cf05fc1b347f73b
MB-Development <mathieu_broe@yahoo.com>
2023-02-13 14:50:28 +0100

    clean

--- code-retreat/TicTacToeTests.cs
+++ code-retreat/TicTacToeTests.cs
@@ -4,44 +4,10 @@ namespace code_retreat;
 
 public class TicTacToeTests
 {
-    [Fact]
-    public void ShouldResetBoard_WhenStartNewGame()
-    {
-        var game = new TicTacToe();
-        
-        game.StartNewGame();
-        
-        Assert.Equal("_________", game.GetBoard());
-    }
     
-    [Fact]
-    public void ShouldPlaceMoveAtLocation_WhenAddMoveToBoardAtLocationCalled()
-    {
-        var game = new TicTacToe();
-        
-        game.StartNewGame();
-        game.AddMoveToBoard(2, IsX: true);
-        
-        Assert.Equal((byte)2, byte.Parse(game.GetBoard().Substring(2,1)));
-    }
 }
 
 public class TicTacToe
 {
-    private string _board;
-    public void StartNewGame()
-    {
-        _board = "_________";
-    }
-
-
-    public string GetBoard()
-    {
-        return _board;
-    }
-
-    public void AddMoveToBoard(byte location, bool IsX)
-    {
-        IsX ? _board[location] = 'X' : _board[location] = 'O';
-    }
+    
 }
