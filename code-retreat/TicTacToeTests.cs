fd900a9993cbed9b63a2b308854f239b851369c8
MB-Development <mathieu_broe@yahoo.com>
2023-02-13 16:22:53 +0100

    AllItemsInRowTest

--- code-retreat/TicTacToeTests.cs
+++ code-retreat/TicTacToeTests.cs
@@ -4,10 +4,21 @@ namespace code_retreat;
 
 public class TicTacToeTests
 {
-   
+    [Fact]
+    public void x()
+    {
+        var game = new TicTacToe();
+        game.AllItemsInRowAreEquasl();
+        Assert.True(game.HasWinner);
+    }
 }
 
 public class TicTacToe
 {
-    
+    public void AllItemsInRowAreEquasl()
+    {
+        HasWinner = true;
+    }
+
+    public bool HasWinner { get; set; }
 }
